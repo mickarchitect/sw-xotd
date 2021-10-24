79d5f9b409aec4b5562e5292d31e9a3373a80688
Mick McMahon <mickarchitect@gmail.com>
2021-10-23 21:40:13 -0600

    Done with initial

---
+++ xotd.py
@@ -0,0 +1,16 @@
+import streamlit as st
+
+st.title('Inspiration of the Day')
+
+# Table with Inspirational Items
+
+st.markdown("""
+| What/Link | Description | Subscription Link |
+| --------- | ----------- | ----------------- |
+| [APOD](https://apod.nasa.gov/apod/astropix.html)  | NASA Astronomy Picture of the Day | [link]() |
+| [Nat Geo Pic of Day](https://www.natgeotv.com/int/photo-of-the-day)  | National Geographic Picture of the Day | [link]() |
+| [Spot the Differences](https://www.spot-differences.com/picture-of-the-day/)  | Spot The Differences - from USA Dyslexia Research | [link]() |
+| [Quotes of the Day from BrainyQuotes](https://www.brainyquote.com/quote_of_the_day) | Several quotes per day about life, love, humor and more | [link]() |
+""")
+
+st.write('below the table')
